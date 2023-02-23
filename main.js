b50d838c4c9f1449222ec111b51e458b62cb75e5
Drew <drc920630@gmail.com>
2023-02-23 14:27:47 +0900

    뮤직플레이어 완성

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ const deg = 360 / articleLength;
 
 const names = ["cardio", "groove", "happy", "light", "lily", "limes", "pop", "swing"];
 
-
-
 for (let i = 0; i < articleLength; i += 1) {
   articleFrame[i].style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
 
   const pic = articleFrame[i].querySelector(".pic");
-  pic.style.backgroundImage = `url(./img/${names[i]}.jpg)`
+  pic.style.backgroundImage = `url(./img/${names[i]}.jpg)`;
+
+  const title = articleFrame[i].querySelector(".text > h2");
+  title.innerHTML = `${names[i]}`;
+
+  //음악파일 일괄적용
+  const audio = document.createElement("audio");
+  audio.setAttribute("src", `./music/${names[i]}.mp3`);
+  audio.setAttribute("loop", "loop");
+  articleFrame[i].append(audio);
 }
 
 
@@ -32,6 +39,8 @@ prev.addEventListener("click", () => {
   }
   for (let el of articleFrame) {
     el.classList.remove("on");
+    // el.querySelector("audio").pause();
+    // el.querySelector(".pic").classList.remove('on');
   }
   articleFrame[active].classList.add("on");
 })
@@ -46,6 +55,48 @@ next.addEventListener("click", () => {
   }
   for (let el of articleFrame) {
     el.classList.remove("on");
+    // el.querySelector("audio").pause();
+    // el.querySelector(".pic").classList.remove('on');
   }
   articleFrame[active].classList.add("on");
 })
+
+//cd회전
+let before = 0; //이전 위치 기억용
+
+for (let el of articleFrame) {
+  const play = el.querySelector(".play")
+  const pause = el.querySelector(".pause")
+  const reroad = el.querySelector(".reroad")
+
+  play.addEventListener("click", (e) => {
+    if (before === 0) {
+      before = e.target;
+    } else if (before !== e.target) {
+      before.closest("article").querySelector(".pic").classList.remove("on");
+      before.closest("article").querySelector("audio").pause();
+      before = e.target;
+    }
+
+    el.querySelector(".pic").classList.add('on');
+    el.querySelector("audio").play();
+  })
+
+  pause.addEventListener("click", (e) => {
+    e.target.closest("article").querySelector(".pic").classList.remove("on");
+    e.target.closest("article").querySelector("audio").pause();
+  })
+
+  reroad.addEventListener("click", (e) => {
+    if (before === 0) {
+      before = e.target;
+    } else if (before !== e.target) {
+      before.closest("article").querySelector(".pic").classList.remove("on");
+      before.closest("article").querySelector("audio").pause();
+      before = e.target;
+    }
+    el.querySelector(".pic").classList.add("on");
+    el.querySelector("audio").load();
+    el.querySelector("audio").play();
+  })
+}
